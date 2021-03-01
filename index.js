25e8f3999bb49f8e2277201243631680d7510c99
Michelle Hey <48541701+HeyMichelle@users.noreply.github.com>
2021-03-01 17:48:45 -0500

    MVP Completed (#1)
    * Assignment completed without stretch goals
    * Final changes and saved

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Airplane.prototype.takeOff = function () {
 };
 Airplane.prototype.land = function () {
   this.isFlying = false;
-};
+};   
 
 
 /*
@@ -39,9 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = []
 }
+
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood)
+  };
+}
+
+Person.prototype.poop = function() {
+  this.stomach.length = 0;
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
+
+
 
 /*
   TASK 2
@@ -57,8 +75,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+    this.tank = 0,
+    this.odometer = 0,
+    this.model = model,
+    this.milesPerGallon = milesPerGallon
+// why do we also need to put the attributes below the initialized instances/arguments (tank, odometer, why are these not arguments?)
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons
 }
 
 /*
@@ -68,18 +94,34 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age) 
+    // this.name = name,
+    // this.age = age,
+    this.favoriteToy = favoriteToy
 }
 
-/* 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
+
+
+  // Baby.prototype.play() = function() {
+  // //   return `Playing with ${this.favoriteToy}`;
+  // // }
+
+  // baby.prototype = Object.create(Person.prototype);
+
+ 
+/*  
   TASK 4
 
-  In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  In your own words explain the four principles for the "this" keyword below: this keyword specifies where a function is invoked
+  1. global: refers to the window/console
+  2. implicit: refers to specific object you are calling method on, uses dot-notation
+  3. new-binding: a new instance
+  4. explicit: pointing to something with a different value, uses .call(), .apply(), .bind()
 */
 
 
